de6505689669e246bb6f5185df1b2b44e068a803
Cevher <cevherkarakoc@live.com>
2019-04-09 15:36:59 +0300

    Add version constant

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+const pjson = require('./package.json');
+
+module.exports.VERSION = pjson.version;
 module.exports.Vector = require("./src/vector");
 module.exports.Matrix = require("./src/matrix");
