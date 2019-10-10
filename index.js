4eebd49a762a7e08d710eb9019e3bfe9ab05f5ba
cevherkarakoc <cevherkarakoc@live.com>
2019-10-10 12:14:12 +0300

    Remove TODOS

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const pjson = require('./package.json');
 module.exports.VERSION = pjson.version;
 module.exports.Vector = require("./src/vector");
 module.exports.Matrix = require("./src/matrix");
-
-// TODO Vector.swizzle, Vector.fromAngle, Vector.toAngle, Vector.rotate docs
